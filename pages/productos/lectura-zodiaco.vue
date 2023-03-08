8dce5e76558a7904a781982ec7d25cba9475d1b5
DavidCaba6194 <cabayosoyasi@gmail.com>
2023-03-08 13:02:27 +0100

    Página de lectura del zodiaco

---
+++ pages/productos/lectura-zodiaco.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <Menu/>
+    <div>Lectura del zodiaco</div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Lectura del zodiaco',
+  head: {
+    title: 'Lectura del zodiaco - Oráculo Gratis',
+    htmlAttrs: {
+      lang: 'es'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Recibe una lectura del zodiaco en tu email totalmente gratis canjeando tus puntos.'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.ico' }]
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  background-color: #343541;
+  font-family: 'Roboto', sans-serif;
+  color: #ffffff;
+  padding: 10px;
+}
+
+a {
+  text-decoration: none;
+  color: #ffffff;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
